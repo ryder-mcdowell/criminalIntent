2be837be33e5694094de62c087159ad80996c331
ryder-mcdowell <ryder.mcdowell@gmail.com>
2018-03-12 21:30:55 -0700

    Add create table

--- java/com/bignerdranch/android/criminalintent/CrimeLab.java
+++ java/com/bignerdranch/android/criminalintent/CrimeLab.java
@@ -1,6 +1,8 @@
 package com.bignerdranch.android.criminalintent;
 
 import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import com.bignerdranch.android.criminalintent.database.CrimeBaseHelper;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,6 +12,8 @@ public class CrimeLab {
     private static CrimeLab sCrimeLab;
 
     private List<Crime> mCrimes;
+    private Context mContext;
+    private SQLiteDatabase mDatabase;
 
     public static CrimeLab get(Context context) {
         if (sCrimeLab == null) {
@@ -20,6 +24,8 @@ public class CrimeLab {
     }
 
     private CrimeLab(Context context) {
+        mContext = context.getApplicationContext();
+        mDatabase = new CrimeBaseHelper(mContext).getWritableDatabase();
         mCrimes = new ArrayList<>();
     }
 
